5f9fe5b10829c3b462d9eb232aebcb15d6f076b1
aadams22 <ashleighadams@Ashleighs-MacBook-Air.local>
2016-03-22 19:31:12 -0400

    adding devise index css

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,9 +1,16 @@
 class PetsController < ApplicationController
   def index
-
+    if current_user
+        puts "I'm logged in as #{current_user.email}"
+    else
+        puts "I'm not logged in"
+    end
   end
 
   def show
+    id = current_user.id
+    user = User.find(id)
 
+    p user
   end
 end
