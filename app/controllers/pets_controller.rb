37e5d223d13e7e051771f73df74881e3154ab783
aadams22 <ashleighadams@Ashleighs-MacBook-Air.local>
2016-03-23 11:46:29 -0400

    clicking oli and olive routes

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,6 @@
 class PetsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     # @pet = Pet.find(params[:id])
     if current_user
@@ -14,4 +16,57 @@ class PetsController < ApplicationController
 
     p user
   end
+
+def createOli
+  puts "Create Oli!"
+  mypet = Pet.new({
+    pet_name: 'Oli',
+    type: 'dog',
+    color: 'blonde',
+    hunger_level: '0',
+    loved_level: '100'
+    })
+
+
+  mypet << current_user
+
+  if mypet.save
+    redirect_to show_path(mypet)
+  else
+    render :action => :new
+  end
+
+  redirect_to '/pets'
+
+end
+
+def createOlive
+  puts "createOlive!"
+  new_friend = {
+    pet_name: 'Olive',
+    type: 'cat',
+    color: 'grey',
+    hunger_level: '0',
+    loved_level: '100'
+  }
+
+  mypet = Pet.new(new_friend)
+  mypet << current_user
+
+  if mypet.save
+    redirect_to show_path(mypet)
+  else
+    render :action => :new
+  end
+
+  redirect_to '/pets'
+
+end
+
+
+def new
+end
+
+
+
 end
